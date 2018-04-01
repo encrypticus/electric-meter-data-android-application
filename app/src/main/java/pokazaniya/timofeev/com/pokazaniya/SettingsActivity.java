64d2d9fa70039d6aa25e64faf9a399c4a7946dc2
Charli Santiago <badcoderspaik@gmail.com>
2018-04-01 22:26:33 +0600

    bugs fixed

--- app/src/main/java/pokazaniya/timofeev/com/pokazaniya/SettingsActivity.java
+++ app/src/main/java/pokazaniya/timofeev/com/pokazaniya/SettingsActivity.java
@@ -1,6 +1,9 @@
 package pokazaniya.timofeev.com.pokazaniya;
 import android.support.v7.app.AppCompatActivity;
 import android.os.*;
+import android.view.View;
+
+import dialogs.AddTpSettingsDialog;
 
 public class SettingsActivity extends AppCompatActivity
 {
@@ -11,6 +14,12 @@ public class SettingsActivity extends AppCompatActivity
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings_layout);
+	}
+
+
+	public void showAddTpSettingsDialog(View view){
+		AddTpSettingsDialog addTpSettingsDialog = new AddTpSettingsDialog();
+		addTpSettingsDialog.show(getSupportFragmentManager(), "tpSettingsDialog");
 	}
 	
 }
